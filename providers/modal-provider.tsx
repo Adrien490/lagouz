1336da5273ebd26b24b70659eb237d6ee9c194d2
Adrien <adrien.poirier@oui-connect.fr>
2024-06-29 22:50:46 +0200

    Update modal-provider.tsx

--- providers/modal-provider.tsx
+++ providers/modal-provider.tsx
@@ -3,13 +3,17 @@
 import FAQDrawer from "@/app/games/_components/faq-drawer";
 import SearchDialog from "@/app/games/_components/search-dialog";
 import ConfirmationDialog from "@/components/confirmation-dialog";
+import { Loader } from "@/components/ui/loader";
+import { Suspense } from "react";
 
 const ModalProvider = () => {
 	return (
 		<>
 			<FAQDrawer />
 			<ConfirmationDialog />
-			<SearchDialog />
+			<Suspense fallback={<Loader />}>
+				<SearchDialog />
+			</Suspense>
 		</>
 	);
 };
