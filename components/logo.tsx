d55261581ad7b7c677ea0a1fed6277e2d195c0e9
Adrien <adrien.poirier@oui-connect.fr>
2024-06-30 19:42:20 +0200

    Update logo.tsx

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,15 +1,16 @@
 "use client";
 
 import Image from "next/image";
+import Link from "next/link";
 
 const Logo = () => {
 	return (
-		<div className="flex items-center space-x-2">
+		<Link href="/" className="flex items-center space-x-2">
 			<div className="relative w-[65px] h-[40px]">
 				<Image src="/logo.png" alt="La Gouz" fill />
 			</div>
 			<h1 className="text-sm italic font-bold">La Gouz</h1>
-		</div>
+		</Link>
 	);
 };
 
